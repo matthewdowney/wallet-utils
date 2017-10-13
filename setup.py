708319516d1b99a4439b6b96a41da142b4b36a8f
Matthew Downey <matthewdowney20@gmail.com>
2017-10-13 12:54:59 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='xpub-utils',
+    version='0.0.0',
+    packages=['xpub'],
+    url='',
+    license='MIT',
+    author='Matthew Downey',
+    author_email='matthewdowney20@gmail.com',
+    description='Utilities for dealing with extended public keys (xpubs) in HD crypto wallets.'
+)
