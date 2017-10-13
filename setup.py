d4de7badb7a58c137589ff83f7f6f0195fa9773c
Matthew Downey <matthewdowney20@gmail.com>
2017-10-13 16:39:26 +0200

    Added requirements to setup.py

--- setup.py
+++ setup.py
@@ -8,5 +8,8 @@ setup(
     license='MIT',
     author='Matthew Downey',
     author_email='matthewdowney20@gmail.com',
-    description='Utilities for dealing with extended public keys (xpubs) in HD crypto wallets.'
+    description='Utilities for dealing with extended public keys (xpubs) in HD crypto wallets. '
+                'Wraps the excellent bip32utils library, which does the ECDSA & key derivation heavy lifting, to '
+                'provide a function wrappers & support different address types.',
+    requires=['bip32utils', 'pysha3']
 )
