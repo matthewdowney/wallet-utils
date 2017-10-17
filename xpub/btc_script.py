e79ac0ac030648224ff23f6194206a2f7169795a
Matthew Downey <matthewdowney20@gmail.com>
2017-10-17 18:55:04 +0200

    Added utils for dealing with bitcoin scripting + tests

---
+++ xpub/btc_script.py
@@ -0,0 +1,36 @@
+"""
+Utilities for Bitcoin scripting--works entirely with byte values.
+"""
+
+# The check multisig opcode
+OP_CHECKMULTISIG = b'\xae'
+
+# The opcode for 1 or True
+OP_1_TRUE = b'\x51'
+
+
+def op_n(int_n):
+    """
+    OP_N where N is a number, an OP_N represents that number.
+    """
+    assert int_n == -1 or 1 <= int_n <= 16, "N in OP_N is in the set: {-1, [1-16]}"
+    op_1 = int(OP_1_TRUE.hex(), 16)    # Fist number coded op is 1
+    hex_val = hex(op_1 + int_n - 1)    # Do integer addition to find the right op code
+    return bytes.fromhex(hex_val[2:])  # Reencode as byte
+
+
+def push_n(int_n):
+    """
+    Opcode to push some number of bytes.
+    """
+    assert 1 <= int_n <= 75, "Can't push more than 75 bytes with PUSH(N) in a single byte."
+    n = hex(int_n)[2:]
+    n = n if len(n) == 2 else '0' + n
+    return bytes.fromhex(n)
+
+
+def push_bytes(byte_data):
+    """
+    A push opcode for the number of bytes provided, and then the provided bytes.
+    """
+    return push_n(len(byte_data)) + byte_data
---
+++ xpub/tests/btc_script_test.py
@@ -0,0 +1,41 @@
+import unittest
+
+from xpub.btc_script import *
+
+
+class TestBtcScriptFunctions(unittest.TestCase):
+
+    def test_op_n(self):
+        # Respective inputs & outputs
+        self.assertListEqual(list(map(op_n, [-1, 1, 2, 16])),
+                             [b'\x4f', OP_1_TRUE, b'\x52', b'\x60'])
+
+    def test_op_n_lt(self):
+        with self.assertRaises(AssertionError):
+            op_n(-2)
+
+    def test_op_n_gt(self):
+        with self.assertRaises(AssertionError):
+            op_n(17)
+
+    def test_push_n(self):
+        self.assertListEqual(list(map(push_n, [1, 5, 10, 16, 75])),
+                             [b'\x01', b'\x05', b'\x0a', b'\x10', b'\x4b'])
+
+    def test_push_n_lt(self):
+        with self.assertRaises(AssertionError):
+            push_n(0)
+
+    def test_push_n_gt(self):
+        with self.assertRaises(AssertionError):
+            push_n(76)
+
+    def test_push_bytes(self):
+        byte_data = b"pushme"
+        push_byte_data = push_bytes(byte_data)
+        self.assertEqual(len(push_byte_data), len(byte_data) + 1)
+        self.assertEqual(push_byte_data[0], len(byte_data))
+        self.assertEqual(push_byte_data[1:], byte_data)
+
+if __name__ == '__main__':
+    unittest.main()
